ad63193ab49ec88a175f79f46ef1b78dcf04dcc7
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2024-03-04 12:30:07 +1300

    added get_expenses

--- a_00_FRC_base.py
+++ a_00_FRC_base.py
@@ -1,6 +1,8 @@
 # import libraries
+import pandas
 
-# Fuctions go here
+
+# Functions go here
 
 # checks that input is either a float or an
 # integer that is more than zero. Takes in custom error message
@@ -11,13 +13,14 @@ def num_check(question, error, num_type):
         try:
             response = num_type(input(question))
 
-            if response <=0:
+            if response <= 0:
                 print(error)
             else:
                 return response
 
         except ValueError:
             print(error)
+
 
 # Checks that user has entered yes / no to question
 def yes_no(question):
@@ -33,6 +36,122 @@ def yes_no(question):
         else:
             print("please answer yes / no")
 
-# main rountine goes here
+
+# checks that user response is not blank
+def not_blank(question, error):
+    valid = False
+    while not valid:
+        response = input(question)
+
+        if response == "":
+            print("{}. \nPlease try again \n".format(error))
+            continue
+
+        return response
 
 
+# currency formatting function
+def currency(x):
+    return "${:.2f}".format(x)
+
+
+# Gets expenses, returns list which has
+# the data frame and sub_total
+def get_expenses(var_fixed):
+    #  set up dictionaries and lists
+
+    item_list = []
+    quantity_list = []
+    price_list = []
+
+    variable_dict = {
+        "Item": item_list,
+        "Quantity": quantity_list,
+        "Price": price_list
+    }
+
+    # loop to get component, quantity adn price
+    item_name = ""
+    while item_name.lower() != "xxx":
+
+        print()
+        # get name, quantity and item
+        item_name = not_blank("Item name: ",
+                              "The component name can't be "
+                              "blank.")
+        if item_name.lower() == "xxx":
+            break
+
+        if var_fixed == "variable":
+            quantity = num_check("Quantity:",
+                                 "The amount must be a whole number"
+                                 "more than zero",
+                                 int)
+        else:
+            quantity = 1
+
+        price = num_check("How much for a single item? $",
+                          "The price must be a number <more "
+                          "than 0>", float)
+
+        # add item, quantity and price to lists
+        item_list.append(item_name)
+        quantity_list.append(quantity)
+        price_list.append(price)
+
+    expense_frame = pandas.DataFrame(variable_dict)
+    expense_frame = expense_frame.set_index('Item')
+
+    # Calculate cost of each component
+    expense_frame['Cost'] = expense_frame['Quantity'] \
+                            * expense_frame['Price']
+
+    # Find sub_total
+    sub_total = expense_frame['Cost'].sum()
+
+    # Currency Formatting (uses currency function)
+    add_dollars = ['Price', 'Cost']
+    for item in add_dollars:
+        expense_frame[item] = expense_frame[item].apply(currency)
+
+    return [expense_frame, sub_total]
+
+
+def expense_print(heading, frame, subtotal):
+    print()
+    print("**** {} Costs ****".format(heading))
+    print(frame)
+    print()
+    print("{} Costs: ${:.2f}".format(heading, subtotal))
+    return ""
+
+
+# ******* main routine goes here *******
+# Get user data
+product_name = not_blank("Product name: ", "The product name can't be blank. ")
+print()
+print("Please enter your variable costs below...")
+# get variable costs
+variable_expenses = get_expenses("variable")
+variable_frame = variable_expenses[0]
+variable_sub = variable_expenses[1]
+
+print()
+have_fixed = yes_no("Do you have fixed costs (y/n)? ")
+if have_fixed == "yes":
+    # Get fixed costs
+    fixed_expenses = get_expenses("fixed")
+    fixed_frame = fixed_expenses[0]
+    fixed_sub = fixed_expenses[1]
+else:
+    fixed_sub = 0
+
+# ***** Printing Area *****
+
+print()
+print("***** Fund Raising - {} *****".format(product_name))
+print()
+expense_print("Variable", variable_frame, variable_sub)
+
+if have_fixed == "yes":
+    expense_print("Fixed", fixed_frame[['Cost']], fixed_sub)
